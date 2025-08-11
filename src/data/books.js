e2c48abe8ec32373b438bbd653bff44cfe3905c9
Cong Hieu <hieuho250403@gmail.com>
2025-08-11 10:18:11 +0700

    Tach file data + làm countdowntimer

---
+++ src/data/books.js
@@ -0,0 +1,365 @@
+// Import images for book data
+import slider1 from '../images/slider_item_1_image.jpg';
+import slider2 from '../images/slider_item_2_image.jpg';
+import slider3 from '../images/slider_item_3_image.jpg';
+import slider5 from '../images/slider_item_5_image.jpg';
+
+// New Books Data
+export const newBooks = [
+    {
+        id: 1,
+        title: "Sách mới 1",
+        author: "Tác giả 1",
+        price: 150000,
+        image: slider1
+    },
+    {
+        id: 2,
+        title: "Sách mới 2",
+        author: "Tác giả 2",
+        price: 180000,
+        image: slider2
+    },
+    {
+        id: 3,
+        title: "Sách mới 3",
+        author: "Tác giả 3",
+        price: 120000,
+        image: slider3
+    },
+    {
+        id: 4,
+        title: "Sách mới 4",
+        author: "Tác giả 4",
+        price: 200000,
+        image: slider5
+    },
+    {
+        id: 5,
+        title: "Sách mới 5",
+        author: "Tác giả 5",
+        price: 160000,
+        image: slider1
+    },
+    {
+        id: 6,
+        title: "Sách mới 6",
+        author: "Tác giả 6",
+        price: 140000,
+        image: slider2
+    },
+    {
+        id: 7,
+        title: "Sách mới 7",
+        author: "Tác giả 7",
+        price: 170000,
+        image: slider3
+    },
+    {
+        id: 8,
+        title: "Sách mới 8",
+        author: "Tác giả 8",
+        price: 190000,
+        image: slider5
+    }
+];
+
+// Top Selling Books Data
+export const topSellingBooks = [
+    {
+        id: 1,
+        title: "Sách bán chạy 1",
+        author: "Tác giả 1",
+        price: 250000,
+        image: slider1
+    },
+    {
+        id: 2,
+        title: "Sách bán chạy 2",
+        author: "Tác giả 2",
+        price: 280000,
+        image: slider2
+    },
+    {
+        id: 3,
+        title: "Sách bán chạy 3",
+        author: "Tác giả 3",
+        price: 220000,
+        image: slider3
+    },
+    {
+        id: 4,
+        title: "Sách bán chạy 4",
+        author: "Tác giả 4",
+        price: 300000,
+        image: slider5
+    },
+    {
+        id: 5,
+        title: "Sách bán chạy 5",
+        author: "Tác giả 5",
+        price: 260000,
+        image: slider1
+    },
+    {
+        id: 6,
+        title: "Sách bán chạy 6",
+        author: "Tác giả 6",
+        price: 240000,
+        image: slider2
+    },
+    {
+        id: 7,
+        title: "Sách bán chạy 7",
+        author: "Tác giả 7",
+        price: 270000,
+        image: slider3
+    },
+    {
+        id: 8,
+        title: "Sách bán chạy 8",
+        author: "Tác giả 8",
+        price: 290000,
+        image: slider5
+    }
+];
+
+// Life Skills Books Data
+export const lifeSkillsBooks = [
+    {
+        id: 1,
+        title: "Kỹ năng giao tiếp",
+        author: "Tác giả A",
+        price: 180000,
+        image: slider1
+    },
+    {
+        id: 2,
+        title: "Quản lý thời gian",
+        author: "Tác giả B",
+        price: 160000,
+        image: slider2
+    },
+    {
+        id: 3,
+        title: "Tư duy tích cực",
+        author: "Tác giả C",
+        price: 140000,
+        image: slider3
+    },
+    {
+        id: 4,
+        title: "Lãnh đạo hiệu quả",
+        author: "Tác giả D",
+        price: 200000,
+        image: slider5
+    },
+    {
+        id: 5,
+        title: "Kỹ năng thuyết trình",
+        author: "Tác giả E",
+        price: 170000,
+        image: slider1
+    },
+    {
+        id: 6,
+        title: "Giải quyết xung đột",
+        author: "Tác giả F",
+        price: 150000,
+        image: slider2
+    },
+    {
+        id: 7,
+        title: "Quản lý stress",
+        author: "Tác giả G",
+        price: 130000,
+        image: slider3
+    },
+    {
+        id: 8,
+        title: "Kỹ năng đàm phán",
+        author: "Tác giả H",
+        price: 190000,
+        image: slider5
+    }
+];
+
+// Children Books Data
+export const childrenBooks = [
+    {
+        id: 1,
+        title: "Truyện cổ tích Việt Nam",
+        author: "Tác giả I",
+        price: 120000,
+        image: slider1
+    },
+    {
+        id: 2,
+        title: "Học toán qua truyện",
+        author: "Tác giả J",
+        price: 100000,
+        image: slider2
+    },
+    {
+        id: 3,
+        title: "Khám phá khoa học",
+        author: "Tác giả K",
+        price: 110000,
+        image: slider3
+    },
+    {
+        id: 4,
+        title: "Rèn luyện tính cách",
+        author: "Tác giả L",
+        price: 95000,
+        image: slider5
+    },
+    {
+        id: 5,
+        title: "Truyện tranh giáo dục",
+        author: "Tác giả M",
+        price: 85000,
+        image: slider2
+    },
+    {
+        id: 6,
+        title: "Học tiếng Anh vui nhộn",
+        author: "Tác giả N",
+        price: 90000,
+        image: slider1
+    },
+    {
+        id: 7,
+        title: "Sách tô màu sáng tạo",
+        author: "Tác giả O",
+        price: 75000,
+        image: slider3
+    },
+    {
+        id: 8,
+        title: "Truyện ngụ ngôn hay",
+        author: "Tác giả P",
+        price: 80000,
+        image: slider5
+    }
+];
+
+// Business Books Data
+export const businessBooks = [
+    {
+        id: 1,
+        title: "Khởi nghiệp thành công",
+        author: "Tác giả Q",
+        price: 250000,
+        image: slider1
+    },
+    {
+        id: 2,
+        title: "Marketing hiện đại",
+        author: "Tác giả R",
+        price: 280000,
+        image: slider2
+    },
+    {
+        id: 3,
+        title: "Quản lý tài chính",
+        author: "Tác giả S",
+        price: 220000,
+        image: slider3
+    },
+    {
+        id: 4,
+        title: "Chiến lược kinh doanh",
+        author: "Tác giả T",
+        price: 300000,
+        image: slider5
+    },
+    {
+        id: 5,
+        title: "Bán hàng hiệu quả",
+        author: "Tác giả U",
+        price: 260000,
+        image: slider1
+    },
+    {
+        id: 6,
+        title: "Quản trị nhân sự",
+        author: "Tác giả V",
+        price: 240000,
+        image: slider2
+    },
+    {
+        id: 7,
+        title: "Đầu tư thông minh",
+        author: "Tác giả W",
+        price: 270000,
+        image: slider3
+    },
+    {
+        id: 8,
+        title: "Xây dựng thương hiệu",
+        author: "Tác giả X",
+        price: 290000,
+        image: slider5
+    }
+];
+
+// Literature Books Data
+export const literatureBooks = [
+    {
+        id: 1,
+        title: "Truyện Kiều",
+        author: "Nguyễn Du",
+        price: 180000,
+        image: slider1
+    },
+    {
+        id: 2,
+        title: "Chí Phèo",
+        author: "Nam Cao",
+        price: 120000,
+        image: slider2
+    },
+    {
+        id: 3,
+        title: "Số đỏ",
+        author: "Vũ Trọng Phụng",
+        price: 150000,
+        image: slider3
+    },
+    {
+        id: 4,
+        title: "Tắt đèn",
+        author: "Ngô Tất Tố",
+        price: 140000,
+        image: slider5
+    },
+    {
+        id: 5,
+        title: "Bỉ vỏ",
+        author: "Nguyên Hồng",
+        price: 130000,
+        image: slider1
+    },
+    {
+        id: 6,
+        title: "Vợ nhặt",
+        author: "Nguyên Hồng",
+        price: 110000,
+        image: slider2
+    },
+    {
+        id: 7,
+        title: "Hai đứa trẻ",
+        author: "Thạch Lam",
+        price: 100000,
+        image: slider3
+    },
+    {
+        id: 8,
+        title: "Những ngày thơ ấu",
+        author: "Nguyên Hồng",
+        price: 95000,
+        image: slider5
+    }
+];
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,6 +7,16 @@ import slider2 from '../images/slider_item_2_image.jpg';
 import slider3 from '../images/slider_item_3_image.jpg';
 import slider5 from '../images/slider_item_5_image.jpg';
 
+// Import book data from separate file
+import {
+    newBooks,
+    topSellingBooks,
+    lifeSkillsBooks,
+    childrenBooks,
+    businessBooks,
+    literatureBooks
+} from '../data/books';
+
 const HomePage = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
     const [currentNewBooksSlide, setCurrentNewBooksSlide] = useState(0);
@@ -121,361 +131,6 @@ const HomePage = () => {
             currency: 'VND'
         }).format(price);
     };
-
-    // Hardcoded data for new sections
-    const newBooks = [
-        {
-            id: 1,
-            title: "Sách mới 1",
-            author: "Tác giả 1",
-            price: 150000,
-            image: slider1
-        },
-        {
-            id: 2,
-            title: "Sách mới 2",
-            author: "Tác giả 2",
-            price: 180000,
-            image: slider2
-        },
-        {
-            id: 3,
-            title: "Sách mới 3",
-            author: "Tác giả 3",
-            price: 120000,
-            image: slider3
-        },
-        {
-            id: 4,
-            title: "Sách mới 4",
-            author: "Tác giả 4",
-            price: 200000,
-            image: slider5
-        },
-        {
-            id: 5,
-            title: "Sách mới 5",
-            author: "Tác giả 5",
-            price: 160000,
-            image: slider1
-        },
-        {
-            id: 6,
-            title: "Sách mới 6",
-            author: "Tác giả 6",
-            price: 140000,
-            image: slider2
-        },
-        {
-            id: 7,
-            title: "Sách mới 7",
-            author: "Tác giả 7",
-            price: 170000,
-            image: slider3
-        },
-        {
-            id: 8,
-            title: "Sách mới 8",
-            author: "Tác giả 8",
-            price: 190000,
-            image: slider5
-        }
-    ];
-
-    const topSellingBooks = [
-        {
-            id: 1,
-            title: "Sách bán chạy 1",
-            author: "Tác giả 1",
-            price: 250000,
-            image: slider1
-        },
-        {
-            id: 2,
-            title: "Sách bán chạy 2",
-            author: "Tác giả 2",
-            price: 280000,
-            image: slider2
-        },
-        {
-            id: 3,
-            title: "Sách bán chạy 3",
-            author: "Tác giả 3",
-            price: 220000,
-            image: slider3
-        },
-        {
-            id: 4,
-            title: "Sách bán chạy 4",
-            author: "Tác giả 4",
-            price: 300000,
-            image: slider5
-        },
-        {
-            id: 5,
-            title: "Sách bán chạy 5",
-            author: "Tác giả 5",
-            price: 260000,
-            image: slider1
-        },
-        {
-            id: 6,
-            title: "Sách bán chạy 6",
-            author: "Tác giả 6",
-            price: 240000,
-            image: slider2
-        },
-        {
-            id: 7,
-            title: "Sách bán chạy 7",
-            author: "Tác giả 7",
-            price: 270000,
-            image: slider3
-        },
-        {
-            id: 8,
-            title: "Sách bán chạy 8",
-            author: "Tác giả 8",
-            price: 290000,
-            image: slider5
-        }
-    ];
-
-    const lifeSkillsBooks = [
-        {
-            id: 1,
-            title: "Kỹ năng giao tiếp",
-            author: "Tác giả A",
-            price: 180000,
-            image: slider1
-        },
-        {
-            id: 2,
-            title: "Quản lý thời gian",
-            author: "Tác giả B",
-            price: 160000,
-            image: slider2
-        },
-        {
-            id: 3,
-            title: "Tư duy tích cực",
-            author: "Tác giả C",
-            price: 140000,
-            image: slider3
-        },
-        {
-            id: 4,
-            title: "Lãnh đạo hiệu quả",
-            author: "Tác giả D",
-            price: 200000,
-            image: slider5
-        },
-        {
-            id: 5,
-            title: "Kỹ năng thuyết trình",
-            author: "Tác giả E",
-            price: 170000,
-            image: slider1
-        },
-        {
-            id: 6,
-            title: "Giải quyết xung đột",
-            author: "Tác giả F",
-            price: 150000,
-            image: slider2
-        },
-        {
-            id: 7,
-            title: "Quản lý stress",
-            author: "Tác giả G",
-            price: 130000,
-            image: slider3
-        },
-        {
-            id: 8,
-            title: "Kỹ năng đàm phán",
-            author: "Tác giả H",
-            price: 190000,
-            image: slider5
-        }
-    ];
-
-    const childrenBooks = [
-        {
-            id: 1,
-            title: "Truyện cổ tích Việt Nam",
-            author: "Tác giả I",
-            price: 120000,
-            image: slider1
-        },
-        {
-            id: 2,
-            title: "Học toán qua truyện",
-            author: "Tác giả J",
-            price: 100000,
-            image: slider2
-        },
-        {
-            id: 3,
-            title: "Khám phá khoa học",
-            author: "Tác giả K",
-            price: 110000,
-            image: slider3
-        },
-        {
-            id: 4,
-            title: "Rèn luyện tính cách",
-            author: "Tác giả L",
-            price: 95000,
-            image: slider5
-        },
-        {
-            id: 5,
-            title: "Truyện tranh giáo dục",
-            author: "Tác giả M",
-            price: 85000,
-            image: slider2
-        },
-        {
-            id: 6,
-            title: "Học tiếng Anh vui nhộn",
-            author: "Tác giả N",
-            price: 90000,
-            image: slider1
-        },
-        {
-            id: 7,
-            title: "Sách tô màu sáng tạo",
-            author: "Tác giả O",
-            price: 75000,
-            image: slider3
-        },
-        {
-            id: 8,
-            title: "Truyện ngụ ngôn hay",
-            author: "Tác giả P",
-            price: 80000,
-            image: slider5
-        }
-    ];
-
-    const businessBooks = [
-        {
-            id: 1,
-            title: "Khởi nghiệp thành công",
-            author: "Tác giả Q",
-            price: 250000,
-            image: slider1
-        },
-        {
-            id: 2,
-            title: "Marketing hiện đại",
-            author: "Tác giả R",
-            price: 280000,
-            image: slider2
-        },
-        {
-            id: 3,
-            title: "Quản lý tài chính",
-            author: "Tác giả S",
-            price: 220000,
-            image: slider3
-        },
-        {
-            id: 4,
-            title: "Chiến lược kinh doanh",
-            author: "Tác giả T",
-            price: 300000,
-            image: slider5
-        },
-        {
-            id: 5,
-            title: "Bán hàng hiệu quả",
-            author: "Tác giả U",
-            price: 260000,
-            image: slider1
-        },
-        {
-            id: 6,
-            title: "Quản trị nhân sự",
-            author: "Tác giả V",
-            price: 240000,
-            image: slider2
-        },
-        {
-            id: 7,
-            title: "Đầu tư thông minh",
-            author: "Tác giả W",
-            price: 270000,
-            image: slider3
-        },
-        {
-            id: 8,
-            title: "Xây dựng thương hiệu",
-            author: "Tác giả X",
-            price: 290000,
-            image: slider5
-        }
-    ];
-
-    const literatureBooks = [
-        {
-            id: 1,
-            title: "Truyện Kiều",
-            author: "Nguyễn Du",
-            price: 180000,
-            image: slider1
-        },
-        {
-            id: 2,
-            title: "Chí Phèo",
-            author: "Nam Cao",
-            price: 120000,
-            image: slider2
-        },
-        {
-            id: 3,
-            title: "Số đỏ",
-            author: "Vũ Trọng Phụng",
-            price: 150000,
-            image: slider3
-        },
-        {
-            id: 4,
-            title: "Tắt đèn",
-            author: "Ngô Tất Tố",
-            price: 140000,
-            image: slider5
-        },
-        {
-            id: 5,
-            title: "Bỉ vỏ",
-            author: "Nguyên Hồng",
-            price: 130000,
-            image: slider1
-        },
-        {
-            id: 6,
-            title: "Vợ nhặt",
-            author: "Nguyên Hồng",
-            price: 110000,
-            image: slider2
-        },
-        {
-            id: 7,
-            title: "Hai đứa trẻ",
-            author: "Thạch Lam",
-            price: 100000,
-            image: slider3
-        },
-        {
-            id: 8,
-            title: "Những ngày thơ ấu",
-            author: "Nguyên Hồng",
-            price: 95000,
-            image: slider5
-        }
-    ];
 
     return (
         <div className="app">
@@ -651,37 +306,39 @@ const HomePage = () => {
                 {/* Flash Sale Section with Countdown */}
                 <div className="flash-sale-section">
                     <div className="flash-sale-content">
-                        <div className="flash-sale-info">
-                            <h2 className="flash-sale-title">Sách Bestseller - Giảm 50%</h2>
-                            <p className="flash-sale-description">Cuốn sách được yêu thích nhất tháng này với giá cực sốc!</p>
-                            <div className="flash-sale-price">
-                                <span className="original-price">500.000đ</span>
-                                <span className="sale-price">250.000đ</span>
-                            </div>
-                            <div className="countdown-timer">
-                                <h3>Kết thúc sau:</h3>
-                                <div className="timer-display">
-                                    <div className="timer-unit">
-                                        <span className="timer-number">{String(timeLeft.hours).padStart(2, '0')}</span>
-                                        <span className="timer-label">Giờ</span>
-                                    </div>
-                                    <span className="timer-separator">:</span>
-                                    <div className="timer-unit">
-                                        <span className="timer-number">{String(timeLeft.minutes).padStart(2, '0')}</span>
-                                        <span className="timer-label">Phút</span>
-                                    </div>
-                                    <span className="timer-separator">:</span>
-                                    <div className="timer-unit">
-                                        <span className="timer-number">{String(timeLeft.seconds).padStart(2, '0')}</span>
-                                        <span className="timer-label">Giây</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <button className="flash-sale-button">Mua ngay</button>
-                        </div>
                         <div className="flash-sale-image">
                             <img src={slider1} alt="Flash Sale Product" />
                             <div className="sale-badge">FLASH SALE</div>
+                            <div className="flash-sale-overlay">
+                                <div className="flash-sale-info">
+                                    <h2 className="flash-sale-title">Sách Bestseller - Giảm 50%</h2>
+                                    <p className="flash-sale-description">Cuốn sách được yêu thích nhất tháng này với giá cực sốc!</p>
+                                    <div className="flash-sale-price">
+                                        <span className="original-price">500.000đ</span>
+                                        <span className="sale-price">250.000đ</span>
+                                    </div>
+                                    <div className="countdown-timer">
+                                        <h3>Kết thúc sau:</h3>
+                                        <div className="timer-display">
+                                            <div className="timer-unit">
+                                                <span className="timer-number">{String(timeLeft.hours).padStart(2, '0')}</span>
+                                                <span className="timer-label">Giờ</span>
+                                            </div>
+                                            <span className="timer-separator">:</span>
+                                            <div className="timer-unit">
+                                                <span className="timer-number">{String(timeLeft.minutes).padStart(2, '0')}</span>
+                                                <span className="timer-label">Phút</span>
+                                            </div>
+                                            <span className="timer-separator">:</span>
+                                            <div className="timer-unit">
+                                                <span className="timer-number">{String(timeLeft.seconds).padStart(2, '0')}</span>
+                                                <span className="timer-label">Giây</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <button className="flash-sale-button">Mua ngay</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
