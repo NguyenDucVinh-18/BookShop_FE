0d92056606028673332fbcfc4b63e4643d7a2ac3
Cong Hieu <hieuho250403@gmail.com>
2025-08-11 10:53:45 +0700

    Demo Detail Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import DetailPage from "./pages/DetailPage";
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/detail" element={<DetailPage />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/pages/DetailPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button, Input, Form, Divider } from 'antd';
+import { FacebookOutlined, GoogleOutlined, MailOutlined, LockOutlined } from '@ant-design/icons';
+import '../styles/LoginPage.css';
+
+
+const DetailPage = () => {
+
+    
+    return (
+        <div>Detail Page</div>
+    );
+};
+
+export default DetailPage;
